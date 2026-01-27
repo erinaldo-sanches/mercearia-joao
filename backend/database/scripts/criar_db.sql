5f7d0154e5f64261b3fcd114ca133444c22f0289
erinaldo-sanches <erinaldo.nascimento@escola.pr.gov.br>
2026-01-26 21:55:53 -0300

    usuário mercearia_dev

--- backend/database/scripts/criar_db.sql
+++ backend/database/scripts/criar_db.sql
@@ -1,3 +1,3 @@
 CREATE DATABASE mercearia_joao_db;
-CREATE USER mercearia_dev WITH PASSWORD 'Dev123';
+CREATE USER mercearia_dev WITH PASSWORD 'dev_ceep';
 GRANT ALL PRIVILEGES ON DATABASE mercearia_joao_db TO mercearia_dev;
